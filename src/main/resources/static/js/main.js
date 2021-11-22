b3dca1b2203a0bfc5260b77ea0947e716471fb25
Wanny <wannydev@gmail.com>
2021-11-22 22:57:01 +0100

    first nav bar and button

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+function RedirectionJavascript() {
+    document.location.href = "/Users/wannyould-amrouche/Documents/mywebsite/cv.html";
+}
+function RedirectionAcceuil() {
+    document.location.href = "/Users/wannyould-amrouche/Documents/mywebsite/index.html";
+}

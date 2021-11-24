b9ce6e9786524168e128b65674cb9d4cb8de2d4d
Wanny <wannydev@gmail.com>
2021-11-24 22:21:17 +0100

    display css

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,15 @@
-function RedirectionJavascript() {
-    document.location.href = "/Users/wannyould-amrouche/Documents/mywebsite/cv.html";
+
+function style84()  {
+ document.getElementById("css").href = "css/main84.css";
+}
+function styleNow()  {
+    document.getElementById("css").href = "css/main.css";
 }
 function RedirectionAcceuil() {
     document.location.href = "/Users/wannyould-amrouche/Documents/mywebsite/index.html";
+}
+if ( navigator.platform.indexOf('Win') != -1 ) {
+    window.document.getElementById("wrapper").setAttribute("class", "windows");
+} else if ( navigator.platform.indexOf('Mac') != -1 ) {
+    window.document.getElementById("wrapper").setAttribute("class", "mac");
 }

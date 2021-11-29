f4e384f27f6bc6b258d18d7afeafedd0f49ed68c
Wanny <wannydev@gmail.com>
2021-11-29 20:41:33 +0100

    update

--- src/main/java/mandrille84/mandrille/Pagecontroller.java
+++ src/main/java/mandrille84/mandrille/Pagecontroller.java
@@ -1,23 +1,32 @@
 package mandrille84.mandrille;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Pagecontroller {
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "pages/home";
     }
+
     @GetMapping("/paint")
-    public String paint(){
+    public String paint() {
         return "pages/paint";
     }
+
+    @GetMapping("/illustration")
+    public String illustration() {
+        return "pages/illustrations";
+    }
+
+    @GetMapping("/wallpaint")
+    public String wallpaint() {
+        return "pages/wallpaint";
+    }
+
     @GetMapping("/wait")
-    public String waiting(){
+    public String waiting() {
         return "pages/wait";
     }
 
